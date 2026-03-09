d5a5d6d5956923ae530edf72efc353c5c91345ab
taiyakihexencoder <162603316+taiyakihexencoder@users.noreply.github.com>
2026-03-09 13:07:01 +0900

    createState忘れ

--- budgeting_app/lib/main.dart
+++ budgeting_app/lib/main.dart
@@ -172,6 +172,11 @@ class HistoryEditPage extends StatefulWidget {
 
   final int _historyId;
   final HistoryEditViewModel _viewModel;
+
+  @override
+  HistoryEditPageState createState() {
+    return HistoryEditPageState();
+  }
 }
 
 class HistoryEditPageState extends State<HistoryEditPage> {
