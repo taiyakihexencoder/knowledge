4c5f2a03376fd957fb105d7c72a2f1f30a8c326b
taiyakihexencoder <162603316+taiyakihexencoder@users.noreply.github.com>
2026-03-10 17:21:35 +0900

    ここはfinalではない

--- budgeting_app/lib/ui/history/widgets/history_filter/history_filter.dart
+++ budgeting_app/lib/ui/history/widgets/history_filter/history_filter.dart
@@ -219,7 +219,7 @@ class HistoryFilterState extends State<HistoryFilter> {
                 ValueListenableBuilder(
                   valueListenable: widget._viewModel.categories,
                   builder: (_, categories, _) {
-                    final List<HistoryFilterCategoryModel> modelList = [];
+                    List<HistoryFilterCategoryModel> modelList = [];
                     for (int selected in selectedCategories.selectedList) {
                       HistoryFilterCategoryModel? model = categories.firstWhereOrNull((model) => model.id == selected);
                       if (model != null) {
@@ -301,7 +301,7 @@ class HistoryFilterState extends State<HistoryFilter> {
                 ValueListenableBuilder(
                   valueListenable: widget._viewModel.shops,
                   builder: (_, shops, _) {
-                    final List<HistoryFilterShopModel> modelList = [];
+                    List<HistoryFilterShopModel> modelList = [];
                     for (int selected in selectedShops.selectedList) {
                       HistoryFilterShopModel? model = shops.firstWhereOrNull((model) => model.id == selected);
                       if (model != null) {
@@ -383,7 +383,7 @@ class HistoryFilterState extends State<HistoryFilter> {
                 ValueListenableBuilder(
                   valueListenable: widget._viewModel.tags,
                   builder: (_, tags, _) {
-                    final List<HistoryFilterTagModel> modelList = [];
+                    List<HistoryFilterTagModel> modelList = [];
                     for (int selected in selectedTags.selectedList) {
                       HistoryFilterTagModel? model = tags.firstWhereOrNull((model) => model.id == selected);
                       if (model != null) {
